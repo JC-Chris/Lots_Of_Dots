0c3164c487adb05e0b8cc6c5868b002853d2a227
Chris Plowman <chris@jhaichrispy.com>
2016-05-20 15:19:22 -0700

    trying forms app with dots in the name.  Initial commit

---
+++ Lots.Of.Dots/MyPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lots.Of.Dots
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
+
